e5c6ea5e8ec228d0469c2fc025b6d9a92252ef4e
Michael Sanders <michaelsand155@gmail.com>
2023-08-25 21:10:45 +1000

    Added configure screen

--- class_objects.py
+++ class_objects.py
@@ -5,10 +5,10 @@ class Button:
     def __init__(self, xpos, ypos, scale, image):
         image_width = image.get_width()
         image_height = image.get_height()
-        self.image = pygame.transform.scale(image, (int(image_width * scale), int(image_height * scale)))
-        self.rect = self.image.get_rect()
+        image = pygame.transform.smoothscale(image, (image_width * scale, image_height * scale))
+        self.rect = image.get_rect()
         self.rect.topleft = (xpos - (image_width / 2 * scale), ypos - (image_height / 2 * scale))
-        WINDOW.blit(self.image, (xpos - (image_width / 2 * scale), ypos - (image_height / 2 * scale)))
+        WINDOW.blit(image, (xpos - (image_width / 2 * scale), ypos - (image_height / 2 * scale)))
 
     def click_down(self):
         mouse_position = pygame.mouse.get_pos()
--- configure_surface.py
+++ configure_surface.py
@@ -6,10 +6,38 @@ from pgfunctions import *
 def load_configure_surface():
     return_image = pygame.image.load("assets/return_button.png").convert_alpha()
     return_button_clicked_down = False
+    
+    two_image = pygame.image.load("assets/two.png").convert_alpha()
+    four_image = pygame.image.load("assets/four.png").convert_alpha()
+    six_image = pygame.image.load("assets/six.png").convert_alpha()
+    eight_image = pygame.image.load("assets/eight.png").convert_alpha()
+    twelve_image = pygame.image.load("assets/twelve.png").convert_alpha()
+    normal_image = pygame.image.load("assets/normal.png").convert_alpha()
+    extended_image = pygame.image.load("assets/extended.png").convert_alpha()
+    player_image = pygame.image.load("assets/player.png").convert_alpha()
+    ai_image = pygame.image.load("assets/ai.png").convert_alpha()
 
     while True:
         clock.tick(FPS)
         pygame.display.update()
+
+        two_button = Button(SURFACE_WIDTH / 2, 150, 0.6, two_image)
+        four_button_field = Button(SURFACE_WIDTH / 2, 100, 0.6, four_image)
+        four_button_level = Button(SURFACE_WIDTH / 2 +  50, 150, 0.6, four_image)
+        six_button = Button(SURFACE_WIDTH / 2 + 100, 150, 0.6, six_image)
+        eight_button = Button(SURFACE_WIDTH / 2 + 50, 100, 0.6, eight_image)
+        twelve_button = Button(SURFACE_WIDTH / 2 + 100, 100, 0.6, twelve_image)
+        normal_button = Button(SURFACE_WIDTH / 2 + 20, 200, 0.6, normal_image)
+        extended_button = Button(SURFACE_WIDTH / 2 + 150, 200, 0.6, extended_image)
+        player_button = Button(SURFACE_WIDTH / 2 + 20, 250, 0.6, player_image)
+        ai_button = Button(SURFACE_WIDTH / 2 + 150, 250, 0.6, ai_image)
+
+
+        field_size_text = draw_text("Field Size: ", SCORES_FONT, ROSE_Q, SURFACE_WIDTH / 4, 100)
+        game_level_text = draw_text("Game Level: ", SCORES_FONT, ROSE_Q, SURFACE_WIDTH / 4, 150)
+        game_type_text = draw_text("Game Type: ", SCORES_FONT, ROSE_Q, SURFACE_WIDTH / 4, 200)
+        game_mode_text = draw_text("Game Mode: ", SCORES_FONT, ROSE_Q, SURFACE_WIDTH / 4, 250)
+
 
         return_button = Button(SURFACE_WIDTH / 2, 600, 1, return_image)
 
